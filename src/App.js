bb6bdbfe3f377b7f31e5cdcd7c73e3d644033593
Inkabella03 <inkabella0303@gmail.com>
2024-01-29 17:10:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+ function App() {
+  const students = [
+    {
+      id: 1,
+      name: "John",
+      email: "john@example.com",
+      batch: 36,
+      subjects: [
+        { subject: "Algoritma", grade: "A" },
+        { subject: "Matematika", grade: "B" },
+        { subject: "Kalkulus", grade: "C" },
+        { subject: "Basis data", grade: "A" },
+      ],
+    },
+    {
+      id: 2,
+      name: "Doe",
+      email: "doe@example.com",
+      batch: 36,
+      subjects: [
+        
+      ],
+    },
+   
+  ];
+}
+
+export default App;
